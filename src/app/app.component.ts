546b1ebe334de920cf703660e57d8d6e597e0762
Tiep Phan <tieppt12@gmail.com>
2018-05-13 15:36:16 +0700

    feat(rxjs): add rxjs demo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { PushPopService } from './push-pop.service';
+import { interval } from 'rxjs';
+import { map, take } from 'rxjs/operators';
 
 @Component({
   selector: 'ptt-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'ptt';
 
   constructor(public pushPopSrv: PushPopService) {}
+
+  ngOnInit() {
+    const timer = interval(500)
+      .pipe(
+        map(res => `return value: ${res}`),
+        take(5)
+      ).subscribe(next => console.log(next));
+  }
 }
